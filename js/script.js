30c47174a531a3c4c151a4da7c6fb472620c70a9
wadams-dev <50535899+wadams-dev@users.noreply.github.com>
2020-06-27 15:07:15 -0400

    add animated navbar files

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const hamburgerIcon = document.querySelector('.hamburger--container');
+const navBar =  document.querySelector('.navbar');
+hamburgerIcon.addEventListener('click', () => {
+    navBar.classList.toggle('change');
+});
